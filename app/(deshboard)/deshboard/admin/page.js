5e816fb2630ff7626b2f5628ce8925a5d0971b33
Md Emon Hossen <mdemong87@gmail.com>
2025-09-30 10:47:06 +0600

    update

--- app/(deshboard)/deshboard/admin/page.js
+++ app/(deshboard)/deshboard/admin/page.js
@@ -1,17 +1,12 @@
 
 
-import SongPlayer from "@/app/componnent/Songplear";
 
 const Page = () => {
     return (
         <div className="h-fit">
-            <h1 className="text-xl text-left pb-6">My Songs:</h1>
+            <h1 className="text-xl text-left pb-6">My Deshborad:</h1>
             <div className="grid grid-cols-4 item-center gap-5">
-                <SongPlayer />
-                <SongPlayer />
-                <SongPlayer />
-                <SongPlayer />
-                <SongPlayer />
+                This is the Admin Deshboard....
             </div>
         </div>
     )
